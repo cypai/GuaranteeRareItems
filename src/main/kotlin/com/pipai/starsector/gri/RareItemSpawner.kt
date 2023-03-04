7f1f24d1abf882980818cc4524ab7e59378280df
Cheng-Yu Pai <mail4charliepai@gmail.com>
2023-03-03 16:37:28 -0800

    works

--- src/main/kotlin/com/pipai/starsector/gri/RareItemSpawner.kt
+++ src/main/kotlin/com/pipai/starsector/gri/RareItemSpawner.kt
@@ -1,12 +1,16 @@
 package com.pipai.starsector.gri
 
 import com.fs.starfarer.api.Global
+import com.fs.starfarer.api.campaign.CargoAPI
 import com.fs.starfarer.api.campaign.CustomCampaignEntityAPI
 import com.fs.starfarer.api.campaign.SectorAPI
+import com.fs.starfarer.api.campaign.SectorEntityToken
+import com.fs.starfarer.api.campaign.SpecialItemData
 import com.fs.starfarer.api.impl.campaign.procgen.DropGroupRow
+import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.special.BaseSalvageSpecial
 import com.fs.starfarer.api.util.Misc
-import com.fs.starfarer.api.util.WeightedRandomPicker
 import com.pipai.starsector.gri.utils.chooseAmount
+import data.scripts.util.MagicCampaign
 
 class RareItemSpawner(private val sector: SectorAPI) {
     val logger = Global.getLogger(this.javaClass)
@@ -14,18 +18,27 @@ class RareItemSpawner(private val sector: SectorAPI) {
 
     fun spawnRareItems() {
         val rareItemIds = allRareItemIds()
+        rareItemIds.forEach { logger.info("${it.specialItemId} ${it.specialItemData}") }
         val entities = sector.validRareItemEntities()
         logger.info("Found ${entities.size} total valid entities.")
-        val chosenSalvageLocations = entities.chooseAmount(rareItemIds.size, Misc.random)
-        chosenSalvageLocations.forEach {
-            it.addTag("gri_loc")
-        }
+        entities.chooseAmount(rareItemIds.size, Misc.random)
+            .zip(rareItemIds)
+            .forEach {  (salvageable, rareItem) ->
+                addRareItem(salvageable, rareItem)
+                salvageable.addTag("gri_salvageable")
+            }
     }
 
-    private fun allRareItemIds(): List<String> {
+    private fun addRareItem(salvageable: SectorEntityToken, item: DropGroupRow) {
+        val cargo = Global.getFactory().createCargo(true)
+        cargo.addItems(CargoAPI.CargoItemType.SPECIAL, SpecialItemData(item.specialItemId, item.specialItemData), 1f)
+
+        BaseSalvageSpecial.addExtraSalvage(cargo, salvageable.memoryWithoutUpdate, -1.0f)
+    }
+
+    private fun allRareItemIds(): List<DropGroupRow> {
         return DropGroupRow.getPicker("rare_tech").items
             .filter { it.isSpecialItem }
-            .map { it.specialItemId }
     }
 
     private fun SectorAPI.validRareItemEntities(): List<CustomCampaignEntityAPI> {
